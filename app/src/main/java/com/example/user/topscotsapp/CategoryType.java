e9bf2e43f660e00981e5a2b54fde2ac4b975e492
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-11-13 13:58:33 +0000

    CategoryType created, tests to be made

--- app/src/main/java/com/example/user/topscotsapp/CategoryType.java
+++ app/src/main/java/com/example/user/topscotsapp/CategoryType.java
@@ -5,4 +5,17 @@ package com.example.user.topscotsapp;
  */
 
 public enum CategoryType {
+    EDINBURGH_CASTLE,
+    ST_GILES_CATHEDRAL,
+    HOLYROOD_PALACE,
+    FORTH_RAIL_BRIDGE,
+    SCOTT_MONUMENT,
+    ROYAL_OBSERVATORY,
+    CODECLAN,
+    CODEBASE,
+    NATIONAL_MONUMENT,
+    NELSON_MONUMENT,
+    DUGALD_STEWART_MONUMENT,
+    CRAIGMILLAR_CASTLE;
 }
+
